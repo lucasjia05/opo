42ed39001661ee554538486319d3fe3fc5e76aef
archqn <lucasjia@ucsb.edu>
2025-10-28 06:30:55 +0000

    minimum viable product working, just need to test and add other models for editing / gradient generation

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def get_args():
     parser.add_argument('--gradient_model', default='gpt-4o-mini')
     parser.add_argument('--edit_model', default='gpt-4o-mini')
     # parser.add_argument('--config', default='default.json')
-    parser.add_argument('--out', default='test_out.txt')
+    parser.add_argument('--out', default='expts/liar_test0.txt')
     parser.add_argument('--max_threads', default=32, type=int)
     parser.add_argument('--temperature', default=0.0, type=float)
 
